d8767e637a9af429e99c883b327cc4bb70b48f4c
zlatangoralija <zlatan@omnitask.ba>
2021-04-06 12:52:33 +0200

    database & models testing

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,3 +26,9 @@ $factory->define(User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+
+$factory->define(\App\Article::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+    ];
+});
---
+++ tests/Integration/ArticleTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Integration;
+
+use App\Article;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ArticleTest extends TestCase
+{
+    //When we use DatabaseTransactions class, all the data that was created in database in this test, will be rolled back (deleted)
+    use DatabaseTransactions;
+
+    public function testFetchesTrendingArticles()
+    {
+
+        factory(Article::class, 3)->create();
+
+        factory(Article::class, 2)->create([
+            'reads' => 10
+        ]);
+
+        $mostPopular = factory(Article::class, 2)->create([
+            'reads' => 20
+        ]);
+
+        $articles = Article::trending($take = 3);
+
+        $this->assertEquals($mostPopular[0]->id, $articles->first()->id);
+        $this->assertCount(3, $articles);
+    }
+}
